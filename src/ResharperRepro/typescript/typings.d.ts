a365d2b7a79429158b29447343ad21ff3526a272
Stephan Oehlert <stephan.oehlert@gmail.com>
2016-08-19 12:00:29 +0200

    Add aspnet core and ng2 project.

---
+++ src/ResharperRepro/typescript/typings.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="../node_modules/rxjs/Rx.d.ts" />
+
+/// <reference path="../node_modules/@angular/common/index.d.ts" />
+/// <reference path="../node_modules/@angular/compiler/index.d.ts" />
+/// <reference path="../node_modules/@angular/core/index.d.ts" />
+/// <reference path="../node_modules/@angular/core/testing.d.ts" />
+/// <reference path="../node_modules/@angular/http/index.d.ts" />
+/// <reference path="../node_modules/@angular/platform-browser/index.d.ts" />
+/// <reference path="../node_modules/@angular/platform-browser-dynamic/index.d.ts" />
+/// <reference path="../node_modules/@angular/router/index.d.ts" />
+/// <reference path="../node_modules/@angular/upgrade/index.d.ts" />
